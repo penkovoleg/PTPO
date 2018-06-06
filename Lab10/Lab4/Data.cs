7f6a99078669d5dc314c6218772028a361d825cc
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:40:07 +0600

    Lab10

---
+++ Lab10/Lab4/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Data
+    {
+        public int[] DataEntry()
+        {
+            FileStream file1 = new FileStream("D:\\ПТПО\\Lab10\\input.txt", FileMode.Open); //создаем файловый поток
+            StreamReader reader = new StreamReader(file1); // создаем «потоковый читатель» и связываем его с файловым потоком 
+            string str = reader.ReadToEnd();
+            reader.Close(); //закрываем поток
+            string[] st = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int size = st.Length;
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(st[i]);
+            }
+            return array;
+        }
+    }
+}
---
+++ Lab10/Lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+            int[] array = data.DataEntry();
+            Calculator calculator = new Calculator();
+            int[] result = calculator.ReshuffleInArray(array);
+            FileStream file1 = new FileStream("D:\\ПТПО\\Lab10\\text.txt", FileMode.Create); //создаем файловый поток
+            StreamWriter writer = new StreamWriter(file1); //создаем «потоковый писатель» и связываем его с файловым потоком 
+            for (int i = 0; i < result.Length; i++)
+            {
+                writer.Write(result[i] + " "); //записываем в файл
+            }
+            writer.Close(); //закрываем поток
+        }
+    }
+}
