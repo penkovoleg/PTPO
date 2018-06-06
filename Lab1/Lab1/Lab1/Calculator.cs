4a9b3489bd1783d912a9bf2ed7b86a53e0b84658
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:05:02 +0600

    Lab1

---
+++ Lab1/Lab1/Lab1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1
+{
+    public class Calculator
+    {
+        public int CalculatorFromYearToCentur(string str)
+        {
+            int year = Convert.ToInt32(str);
+            int size = str.Length;
+            int century = 0;
+            if (size == 1)
+            {
+                century = year + 1;
+            }
+            if (size == 2)
+            {
+                century = year / 10 + 1;
+            }
+            if (size == 3 || size == 4)
+            {
+                century = year / 100 + 1;
+            }
+            return century;
+        }
+    }
+}
---
+++ Lab1/Lab1/Lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год: ");
+            string str = Console.ReadLine();
+            Calculator calculator = new Calculator();
+            int century = calculator.CalculatorFromYearToCentur(str);
+            Console.WriteLine("Столетие: " + century);
+            Console.ReadLine();
+        }
+    }
+}
