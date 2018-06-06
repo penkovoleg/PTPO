c2a0bf87675ece9ad8132cfc9319841b93873af1
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:17:29 +0600

    Lab6

---
+++ Lab6/ConsoleApp1/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class DeleteInArray
+    {
+        public List<int> deletearray(List<int> array)
+        {
+            int end = array[array.Count - 1];
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] % end != 0)
+                {
+                    array.RemoveAt(i);
+                    i--;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Lab6/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int>{ 4, 3, 4, 5, -1, -2, -3, 2};
+            Console.Write("Исходные элементы: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+            DeleteInArray delete = new DeleteInArray();
+            delete.deletearray(array);
+            Console.WriteLine();
+            Console.Write("Элементы после удаления: ");
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
