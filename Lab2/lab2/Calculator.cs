027a307481d8284ccb5f558fdc3b1cb2abda7820
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:08:15 +0600

    Lab2

---
+++ Lab2/lab2/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab2
+{
+    public class Calculator {
+        public int WorkWithStrings(string str) {
+            int result = 0;
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++) {
+                if (str.Length % 2 != 0) {
+                    result = -1;
+                    counter = 1;
+                    break;
+                }
+                if (str[i] == '(')  {
+                    counter++;
+                }
+                else if(str[i] == ')') {
+                    counter--;
+                }
+            }
+            if (counter == 0) {
+                result = 0;
+            }
+            else result = -1;
+            return result;
+        }
+    }
+}
---
+++ Lab2/lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите скобки: ");
+            string str = Console.ReadLine();
+            Calculator calculator = new Calculator();
+            int result = calculator.WorkWithStrings(str);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
