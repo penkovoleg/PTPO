70188d876f8c3c0ca91ebe304c0548af8d28faad
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:15:42 +0600

    Lab4

---
+++ Lab4/Lab4/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4 {
+    public class Calculator {
+        public int[] ReshuffleInArray(int[] array) {
+            int negative = 0;
+            int positive = 0;
+            int counter = 0;
+            int max = 0;
+            int index = 0;
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+            }
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] < negative) {
+                    negative = array[i];
+                    counter = i;
+                }
+                if (array[i] > 0 && array[i] < max) {
+                    max = array[i];
+                    positive = array[i];
+                    index = i;
+                }
+            }
+            array[counter] = positive;
+            array[index] = negative;
+            return array;
+        }
+    }
+}
---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data data = new Data();
+            int[] array = data.DataEntry();
+            Calculator calculator = new Calculator();
+            int[] result = calculator.ReshuffleInArray(array);
+            Console.Write("Массив после замены: ");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.Write(" " + result[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
