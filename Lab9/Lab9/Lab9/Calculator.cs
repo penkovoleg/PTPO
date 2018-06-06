8d40a163070c3101d9e54e90db16ec0e9043e920
penkovoleg <purtrtggggg@gmail.com>
2018-06-07 01:39:20 +0600

    Lab9

---
+++ Lab9/Lab9/Lab9/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    public class Calculator
+    {
+        public int[,] SortInArray(int[,] array, int rows, int columns)
+        {
+            int element = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    for (int x = 0; x < rows; x++)
+                    {
+                        for (int y = 0; y < columns; y++)
+                        {
+                            if (array[i, j] > array[x, y])
+                            {
+                                element = array[i, j];
+                                array[i, j] = array[x, y];
+                                array[x, y] = element;
+                            }
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ Lab9/Lab9/Lab9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 3] {{8, 4, 2}, 
+                                          {6, 3, 5},
+                                          {9, 7, 1}
+            };
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            Console.WriteLine("Массив до сортировки:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Массив после сртировки: ");
+            Calculator calculator = new Calculator();
+            int[,] arr = calculator.SortInArray(array, rows, columns);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
